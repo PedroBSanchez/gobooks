82f762b40c26af4b794fef846f28cdabe73168ab
PedroBSanchez <odi@pop-os.localdomain>
2023-08-22 22:23:12 -0300

    feat : dependencies adding

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
+import "github.com/PedroBSanchez/gobooks.git/config"
+
+
+var (
+	logger *config.Logger
+)
+
 
 func main () {
+
+	logger = config.GetLogger("main")
+
+	
+	// Initialize configs
+	err := config.Init()
+
+	if err != nil {
+		logger.ErrorF("CONFIG initialization error: %v", err)
+		return
+	}
+
+	//Initialize Router
+
+	//router.Initialize()
 
 
 	
